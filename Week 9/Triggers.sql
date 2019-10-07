0d49cb2a957b84f1c0748a884d01b259e0bb694e
JeophreyCoritico <48078050+JeophreyCoritico@users.noreply.github.com>
2019-10-08 10:55:51 +1100

    added sql triggers

--- Week 9/Triggers.sql
+++ Week 9/Triggers.sql
@@ -29,34 +29,76 @@
 drop sequence LogIdSeq
 GO
 
-create sequence LogIdSeq
+create sequence LogIdSeq 
 start with 1
-INCREMENT by 1;
+INCREMENT by 1
+cycle;
 
-drop TRIGGER TR_UPD_Students
+drop TRIGGER TR_INS_Students
 go
 delete from StudentTRIG
 GO
 delete from LogTRIG
 go
 
-Create TRIGGER TR_UPD_Students on StudentTRIG
+Create TRIGGER TR_INS_Students on StudentTRIG 
 for insert
 not for replication
 as
 BEGIN
 insert into LogTRIG (LogID, StudentID, DateTimeChanged, DBUser, OldSurname,
 NewSurname, OldGivename, NewGivename, OldMobile, NewMobile)
-values (LogIdSeq.NEXTVAL, null, null, null, null, null, null, null, null, null) 
-
--- insert into LogTRIG (logID, OldSurname, OldGivename, OldMobile)
--- VALUES (LogIdSeq, null, null, null)
+select Next value for LogIdSeq, i.StudentID, GETDATE(), CURRENT_USER, null, 
+i.Surname, null, i.Givename, null, i.mobile 
+from inserted i
 end 
 GO
 
-insert into StudentTRIG (StudentID, Surname, Givename, mobile)
-values (1, 'jeff', 'jeoph', 000);
+drop TRIGGER TR_INS_Students
 go
+
+create TRIGGER TR_UPD_Students on StudentTRIG
+for update 
+not for replication
+AS
+BEGIN
+insert into LogTRIG (LogID, StudentID, DateTimeChanged, DBUser, OldSurname,
+NewSurname, OldGivename, NewGivename, OldMobile, NewMobile)
+select Next value for LogIdSeq, i.StudentID, GETDATE(), CURRENT_USER, d.Surname, 
+i.Surname, d.Givename, i.Givename, d.mobile, i.mobile 
+from inserted i, deleted d
+END
+
+drop TRIGGER TR_DEL_Students
+GO
+
+create TRIGGER TR_DEL_Students on StudentTRIG
+Instead of DELETE
+AS
+BEGIN
+select 'Students cannot Be Deleted' as [Message]
+end   
+GO
+
+DELETE from StudentTRIG
+GO
+select * from StudentTRIG
+GO
+select * from LogTRIG
+
+insert into StudentTRIG (StudentID, Surname, Givename, mobile)
+values (3, 'test', 'testest', 300);
+go
+select * from StudentTRIG
+GO
+select * from LogTRIG
+
+update StudentTRIG set 
+surname = 'yeet',
+givename = 'test',
+mobile = 911
+where studentID = 3
+GO
 select * from StudentTRIG
 GO
 select * from LogTRIG
